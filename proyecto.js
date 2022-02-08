0123ae85e99d6024ce87b0b9ae3d45a2ebc35165
Victor <victor.lvl47@gmail.com>
2022-02-08 17:04:28 -0600

    convertimos la cantidad de hijos a numero, si los tinene

--- proyecto.js
+++ proyecto.js
@@ -1,8 +1,8 @@
-//Variables utiles 
+//Variables utiles
 //Precio base de la cotización, en quetzales, lo puede cambiar
 var precio_base = 2000
 
-//Valores de los recargos 
+//Valores de los recargos
 var edad_18 = 0.1 // 10%
 var edad_25 = 0.2 // 20%
 var edad_50 = 0.3 // 30%
@@ -18,10 +18,10 @@ var hijos_recargo = 0.2 // 20%
 var recargo = 0
 var recargo_total = 0
 
-//Precio final 
+//Precio final
 var precio_final = 0
 
-//Mensajes de alerta para ingresar datos 
+//Mensajes de alerta para ingresar datos
 var nombre = prompt("Ingrese su nombre, por favor")
 var edad = prompt("¿Cuantos años tiene? Ingrese solamente números ")
 
@@ -31,7 +31,7 @@ var edad_conyuge
 if("SI" == casado.toUpperCase()){
   edad_conyuge = prompt("¿Que edad tiene su esposo/a?", "si/no")
 }
-//convirtiendo las edades ingresadas a números 
+//convirtiendo las edades ingresadas a números
 var edad_numero = parseInt(edad)
 var edad_conyuge_numero = 0
 //convirtiendo la edad del cónyuge si se esta casado/a
@@ -42,29 +42,36 @@ if("SI" == casado.toUpperCase()){
 var hijos = prompt("¿Tiene hijos o hijas?")
 //Comprobamos la cantidad de hijos solamente si los tienen
 var cantidad_hijos
-/**
- * 1. convierta la cantidad de hijos a numero
- */
+var cantidad_hijos_numero = 0
+if ("SI" == hijos.toUpperCase()) {
+  cantidad_hijos = prompt("¿Cuantos hijos/hijas tiene?")
+  /**
+   * 1. convierta la cantidad de hijos a numero
+   */
+  cantidad_hijos_numero = parseInt(cantidad_hijos)
+}
+// mostrar la cantidad de hijos que se ingreso en consola
+// console.log({cantidad_hijos_numero})
 
 //Aquí debe calcular el recargo total basado en las respuestas ingresadas
 
 //Aquí es donde debe de calcular los recargos y el valor final
-//Ejemplo (Debe completar los condicionales): Recargo por edad del asegurado 
+//Ejemplo (Debe completar los condicionales): Recargo por edad del asegurado
 if(edad_numero>=18 && edad_numero<25){
-  //Calculamos el recargo en base a la edad 
+  //Calculamos el recargo en base a la edad
   recargo = precio_base * edad_18
   //Sumamos todos los recargos que hemos obtenido
   recargo_total = recargo_total + recargo
 }
 //aqui puede colocar un else if() con el siguiente rango
 
-/** 
+/**
  * 2. Recargo por la edad del conyuge
  */
 
 /**
- * 3. Recargo por la cantidad de hijos 
- */ 
+ * 3. Recargo por la cantidad de hijos
+ */
 
 
 precio_final = precio_base + recargo_total
@@ -72,4 +79,3 @@ precio_final = precio_base + recargo_total
 alert ("Para el asegurado "+nombre)
 alert ("El recargo total sera de: "+recargo_total)
 alert ("El precio sera de: "+precio_final)
-
